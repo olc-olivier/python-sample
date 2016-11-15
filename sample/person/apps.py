777415a0c2baf003d26caaefbc6b0f3905ff5721
Olivier Chantereau <o.chantereau@itecor.com>
2016-11-15 08:46:07 +0000

    update site1 to person

--- sample/person/apps.py
+++ sample/person/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class Site1Config(AppConfig):
-    name = 'site1'
+class PersonConfig(AppConfig):
+    name = 'person'
--- sample/sample/urls.py
+++ sample/sample/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^site1/', include('site1.urls')),
+    url(r'^person/', include('person.urls')),
     url(r'^admin/', admin.site.urls),
 ]
