7e2e8df2245bf6d5f19307f182131d1524e9f528
Olivier Chantereau <o.chantereau@itecor.com>
2016-11-17 08:50:31 +0000

    Add rest methods.

--- sample/person/views.py
+++ sample/person/views.py
@@ -7,7 +7,6 @@ from .models import Question
 
 # Create your views here.
 def index(request):
-
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
